15b786900b67d2159ae86c0d99755d237b2439dc
speedssster <lastprogamer0@gmail.com>
2021-05-12 04:52:59 +0430

    - fixed bug for sending files

--- teleframe/connection.py
+++ teleframe/connection.py
@@ -1,30 +1,31 @@
 from requests import post
 
 
-class Connection():
-    '''Simple one-time lazy Connection Model for requests'''
+class Connection:
+    """Simple one-time lazy Connection Model for requests"""
 
     url: str = ""
 
     def __init__(self, url: str, bot_token: str) -> None:
-        '''instantiate a `Connection` object with `url`(telegram server) and
-        `Bot Token`'''
+        """instantiate a `Connection` object with `url`(telegram server) and
+        `Bot Token`"""
 
-        self.url = f'{url}/bot{bot_token}/'
+        self.url = f"{url}/bot{bot_token}/"
 
     def send(self, method: str, **kwargs) -> tuple:
-        '''Wrap & send new POST request by given `method` and `kwargs`.
+        """Wrap & send new POST request by given `method` and `kwargs`.
 
         :param method: case-insensitive str from telegram bot api available methods.
         :param \*\*kwargs: Optional arguments that `method` takes.
-        
+
         :return: `tuple(state <bool>, response <JSON-object>)`
-        '''
+        """
 
         try:
-            response = post(self.url + method, json=kwargs)
+            files = kwargs.pop("files", None)
+            response = post(self.url + method, data=kwargs, files=files)
         except Exception as error:
             print(error)
-            return (False, {'error': error})
+            return (False, {"error": error})
         else:
             return (response.ok, response.json())
